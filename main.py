0b1ab9b79d7601276904bcce95fcaa9f9a624c24
mato <tomoya.matou@gmail.com>
2019-02-09 17:05:17 +0900

    2.5

--- main.py
+++ main.py
@@ -1,13 +1,56 @@
+import numpy as np
 
 def AND(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7
-    tmp = x1*w1 + x2*w2
-    if tmp <= theta:
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
         return 0
-    elif tmp > theta:
+    elif tmp > 0:
         return 1
 
-print(AND(0, 0))
-print(AND(0, 1))
-print(AND(1, 0))
-print(AND(1, 1))
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
+        return 0
+    elif tmp > 0:
+        return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
+        return 0
+    elif tmp > 0:
+        return 1
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    return AND(s1, s2)
+
+def PRINTPERCEPTRON():
+    x_list = np.array(
+        [
+            [0, 0],
+            [0, 1],
+            [1, 0],
+            [1, 1]
+        ]
+    )
+    for x in x_list:
+        print('AND(' + str(x[0]) + ', ' + str(x[1]) + '): ' + str(AND(x[0], x[1])))
+    for x in x_list:
+        print('NAND(' + str(x[0]) + ', ' + str(x[1]) + '): ' + str(NAND(x[0], x[1])))
+    for x in x_list:
+        print('OR(' + str(x[0]) + ', ' + str(x[1]) + '): ' + str(OR(x[0], x[1])))
+    for x in x_list:
+        print('XOR(' + str(x[0]) + ', ' + str(x[1]) + '): ' + str(XOR(x[0], x[1])))
+
+PRINTPERCEPTRON()
